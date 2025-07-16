1d5360b82771ff0f8f8661e4ec8f428c7e12ea7f
hsharma0004 <harshmaheshsharma2004@gmail.com>
2025-07-16 14:39:17 +0530

    Updated delete functionality

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import toast from "react-hot-toast";
 import Notecard from "../components/Notecard";
 import api from "../lib/axios";
+import NotesNotFound from "../components/NotesNotFound";
 
 
 const HomePage = () => {
@@ -28,10 +29,12 @@ const HomePage = () => {
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
 
+        {notes.length === 0 && <NotesNotFound />}
+
         {notes.length > 0 && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
-              <Notecard key={note._id} note={note} />
+              <Notecard key={note._id} note={note} setNotes={setNotes} />
             ))}
           </div>
         )}
