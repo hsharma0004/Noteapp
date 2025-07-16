a34a7fb7ca15679104aa99c468e259a8c583f606
hsharma0004 <harshmaheshsharma2004@gmail.com>
2025-07-16 08:06:07 +0530

    Rectified Notecard component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {Route , Routes} from "react-router" ;
+import { Route, Routes } from "react-router";
 import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 import NoteDetail from './pages/NoteDetail'
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <div className="relative h-full w-full">
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
-      
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
